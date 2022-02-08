c9214cbb9eb6e6264cb9c8d2f50b5df94673a0a6
gianlukk994 <34285663+gianlukk994@users.noreply.github.com>
2022-02-08 19:01:23 +0100

    Brew bundle command
    Add --no-quarantine flag to skip gatekeeper checks

--- setup_homebrew.sh
+++ setup_homebrew.sh
@@ -5,3 +5,7 @@ source './utils.sh'
 fancy_echo "\n <<< Starting Homebrew Setup >>> \n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+export HOMEBREW_CASK_OPTS = "--no-quarantine"
+
+brew bundle --verbose

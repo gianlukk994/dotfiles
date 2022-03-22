7e3c44deeda3206b77b6684601d28b25c892ee0b
Gianluca Elia <34285663+gianlukk994@users.noreply.github.com>
2022-03-22 11:42:32 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 source /usr/local/opt/asdf/asdf.fish
 
 set theme_color_scheme dracula
+set -g theme_display_ruby no
 
 # Set variables
 # Syntax highlight for man pages using bat
@@ -10,21 +11,15 @@ export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 # Aliases
 
 # Utilities
-
-alias cdp='cd ~/Projects/Private'
-alias cds='cd ~/Projects/Sisal'
-
 alias addalias='vim ~/.config/fish/config.fish'
 
 # MongoDB service
-
 alias startmongo='brew services start mongodb-community@5.0'
 alias stopmongo='brew services stop mongodb-community@5.0'
+
 fish_add_path /usr/local/opt/sqlite/bin
 
 # Npm utilities
-alias npmpublic='npm set registry https://registry.npmjs.org/'
-alias npmsisal='npm set registry http://10.29.10.4:9082/repository/npm-pam/'
 
 # Git utilites
 alias gch='git checkout'
